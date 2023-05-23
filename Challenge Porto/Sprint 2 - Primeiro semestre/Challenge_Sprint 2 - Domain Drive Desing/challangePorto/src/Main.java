293c16f3beb79fddb7fa0b618515316e6ffb0517
EduardoFoncescaFinardi <129402056+EduardoFoncescaFinardi@users.noreply.github.com>
2023-05-23 19:10:32 -0300

    Add files via upload

---
+++ Challenge Porto/Sprint 2 - Primeiro semestre/Challenge_Sprint 2 - Domain Drive Desing/challangePorto/src/Main.java
@@ -0,0 +1,44 @@
+import br.com.challange.model.*;
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Bicicleta bicicleta;
+
+        int encerrar = 0;
+
+        do {
+            var nomeBicicleta = JOptionPane.showInputDialog("Bicicleta");
+
+            var tipoMaterial = JOptionPane.showInputDialog("Material");
+
+            var tipoPneus = JOptionPane.showInputDialog("Pneus");
+
+            var tipoAro = JOptionPane.showInputDialog("Aro");
+
+            var tipoQuadro = JOptionPane.showInputDialog("Quadro");
+
+            var tipoMarcha = JOptionPane.showInputDialog("Marcha");
+
+            var tipoSuspensao = JOptionPane.showInputDialog("Suspensão");
+
+            var tipoGuidao = JOptionPane.showInputDialog("Guidão");
+
+            bicicleta = new Bicicleta(tipoMaterial, tipoGuidao, tipoAro, tipoPneus, tipoQuadro, tipoMarcha, tipoSuspensao);
+
+            var pergunta = """
+                    Deseja cadastrar mais uma bicicleta?
+                    [ 1 ] SIM
+                    [ 0 ] NÃO
+                    """;
+            encerrar = Integer.parseInt(JOptionPane.showInputDialog(pergunta));
+
+        } while (encerrar > 0);
+
+        JOptionPane.showMessageDialog(null, "Bicicleta cadastrada: " + bicicleta);
+
+        System.exit(0);
+    }
+}
---
+++ Challenge Porto/Sprint 2 - Primeiro semestre/Challenge_Sprint 2 - Domain Drive Desing/challangePorto/src/br/com/challange/model/Bicicleta.java
@@ -0,0 +1,119 @@
+package br.com.challange.model;
+
+public class Bicicleta {
+
+
+    private String nome;
+
+
+
+    private int aro;
+
+    private String material;
+
+    private boolean pedais;
+
+    private String Pneus;
+
+    private String quadro;
+
+    private int marcha;
+
+    private boolean suspensao;
+
+    private String guidao;
+
+    public Bicicleta() {
+    }
+
+    public Bicicleta(int aro, String material, boolean pedais, String pneus, String quadro, int marcha, boolean suspensao, String guidao) {
+        this.aro = aro;
+        this.material = material;
+        this.pedais = pedais;
+        Pneus = pneus;
+        this.quadro = quadro;
+        this.marcha = marcha;
+        this.suspensao = suspensao;
+        this.guidao = guidao;
+    }
+
+    public Bicicleta(String nomeBicicleta) {
+
+    }
+
+    public Bicicleta(String tipoMaterial, String tipoGuidao, String tipoAro, String tipoPneus, String tipoQuadro, String tipoMarcha, String tipoSuspensao) {
+    }
+
+    public int getAro() {
+        return aro;
+    }
+
+    public void setAro(int aro) {
+        this.aro = aro;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public boolean isPedais() {
+        return pedais;
+    }
+
+    public void setPedais(boolean pedais) {
+        this.pedais = pedais;
+    }
+
+    public String getPneus() {
+        return Pneus;
+    }
+
+    public void setPneus(String pneus) {
+        Pneus = pneus;
+    }
+
+    public String getQuadro() {
+        return quadro;
+    }
+
+    public void setQuadro(String quadro) {
+        this.quadro = quadro;
+    }
+
+    public int getMarcha() {
+        return marcha;
+    }
+
+    public void setMarcha(int marcha) {
+        this.marcha = marcha;
+    }
+
+    public boolean isSuspensao() {
+        return suspensao;
+    }
+
+    public void setSuspensao(boolean suspensao) {
+        this.suspensao = suspensao;
+    }
+
+    public String getGuidao() {
+        return guidao;
+    }
+
+    public void setGuidao(String guidao) {
+        this.guidao = guidao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
