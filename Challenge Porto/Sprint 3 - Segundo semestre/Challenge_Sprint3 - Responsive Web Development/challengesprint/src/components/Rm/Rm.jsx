f7a00eb87d5c621a9d29aba5898abf055268b116
Eduardo Foncesca Finardi <129402056+EduardoFoncescaFinardi@users.noreply.github.com>
2024-01-19 12:41:19 -0300

    Add files via upload

---
+++ Challenge Porto/Sprint 3 - Segundo semestre/Challenge_Sprint3 - Responsive Web Development/challengesprint/src/components/Rm/Rm.jsx
@@ -0,0 +1,23 @@
+import './Rm.css'
+
+
+const Plano = () => {
+    return(
+        <div className='corpo'>
+            <div className='box'>
+                <div className='box-rm'>
+                    <p>Felipe Morais - RM 551463 <br/><br/>
+                    João Cardoso - RM 552078 <br/><br/>
+                    Miguel Santos - RM 551640 <br/><br/>
+                    Eduardo Finardi - RM 98624 <br/><br/>
+                    Murilo Munari - RM 94164 <br/><br/>
+                    GitHub: https://github.com/FehMs/RWD-Challenge-3
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Plano;
