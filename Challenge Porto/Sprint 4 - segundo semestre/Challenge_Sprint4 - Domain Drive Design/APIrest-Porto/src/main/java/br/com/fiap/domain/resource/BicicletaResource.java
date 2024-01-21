6f4c62de98ffec2cd859112928478bd16509567f
Eduardo Foncesca Finardi <129402056+EduardoFoncescaFinardi@users.noreply.github.com>
2024-01-21 13:33:36 -0300

    Add files via upload

---
+++ Challenge Porto/Sprint 4 - segundo semestre/Challenge_Sprint4 - Domain Drive Design/APIrest-Porto/src/main/java/br/com/fiap/domain/resource/BicicletaResource.java
@@ -0,0 +1,52 @@
+package br.com.fiap.domain.resource;
+
+import br.com.fiap.domain.entity.Bicicleta;
+import br.com.fiap.domain.service.BicicletaService;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Objects;
+
+@Path("/bicicleta")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class BicicletaResource implements Resource<Bicicleta, Long> {
+
+    @Context
+    UriInfo uriInfo;
+
+     BicicletaService service = new BicicletaService();
+
+    @POST
+    @Override
+    public Response persist(Bicicleta bicicleta) {
+        bicicleta.setId(null);
+        Bicicleta persit = service.persist(bicicleta);
+
+        if (Objects.isNull(persit)) return Response.status(404).build();
+
+        UriBuilder uriBuilder = uriInfo.getBaseUriBuilder();
+        URI uri = uriBuilder.path(String.valueOf(persit.getId())).build();
+
+        return Response.created(uri).entity(persit).build();
+    }
+
+    @GET
+    @Override
+    public Response findAll() {
+        List<Bicicleta> all = service.findAll();
+        return Response.ok(all).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Override
+    public Response findById(@PathParam("id") Long id) {
+        Bicicleta bicicleta = service.findById(id);
+        if (Objects.isNull(bicicleta)) return Response.status(404).build();
+        return Response.ok(bicicleta).build();
+    }
+
+}
